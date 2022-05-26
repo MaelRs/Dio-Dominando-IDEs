7ef1fc162c327ca3c86690da1fb152b929492339
MaelRs <robertdossantos.ismael@gmail.com>
2022-05-26 13:38:19 -0300

    first commit java code.

---
+++ src/com/dio/PrimeiroProjetoJava.java
@@ -0,0 +1,21 @@
+package com.dio;
+
+public class PrimeiroProjetoJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Livros livros=new Livros();
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+
+	}
+
+}
+class Livros {
+	private String nome;
+	private String npag;
+	
+}
